c1a7d0b2f146514da8b2c3cc2d49845ee2c76e77
krocksa <rogeryonatan@gmail.com>
2022-09-11 21:25:54 +0000

    Simple Counter Listerine

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,30 +4,28 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<>
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-sm-2 clock">
-				<i class="far fa-clock"></i>
-				</div>
-				<div class="col-sm-2">
-				</div>
-				<div class="col-sm-2">
-				</div>
-				<div class="col-sm-2">
-				</div>
-				<div class="col-sm-2">
-				</div>
-				<div class="col-sm-2">
-				</div>
-				<div class="col-sm-2">
-				</div>
-			</div>
+		<div class="row">
+		<div class="col-sm-2 clock"><i class="far fa-clock"></i></div>
+		<div class="col-sm-2">{props.digit6 % 10}</div>
+		<div class="col-sm-2">{props.digit5 % 10}</div>
+		<div class="col-sm-2">{props.digit4 % 10}</div>
+		<div class="col-sm-2">{props.digit3 % 10}</div>
+		<div class="col-sm-2">{props.digit2 % 10}</div>
+		<div class="col-sm-2">{props.digit1 % 10}</div>
 		</div>
 		</>
 	);
+	Home.propTypes = {
+		digit1: PropTypes.number,
+		digit2: PropTypes.number,
+		digit3: PropTypes.number,
+		digit4: PropTypes.number,
+		digit5: PropTypes.number,
+		digit6: PropTypes.number
+    };
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,18 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Proptypes from "prop-types";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let count = 0;
+setInterval(function () {
+    const six =   Math.floor(count/100000);
+    const five =  Math.floor(count/10000);
+    const four =  Math.floor(count/1000);
+    const three = Math.floor(count/100);
+    const two =   Math.floor(count/10);
+    const one =   Math.floor(count/1);
+    count ++;
+
+		 ReactDOM.render(<Home digit1={one} digit2={two} digit3={three} digit4={four} digit5={five} digit6={six} />,
+         document.querySelector("#app"));
+},1000);
